66ee5527c3626d387ec4e8595fedd660bbd6dff7
ketankokane94 <ketankokane94@gmail.com>
2019-04-23 10:47:27 -0400

    added memoization

--- homework5/test.py
+++ homework5/test.py
@@ -1,35 +1,41 @@
 import numpy
 
-world = numpy.zeros((3,4))
-world[0,3] = 1
-world[1,3] = -1
+NUMBER_OF_ROWS = 10
+NUMBER_OF_COLS = 10
+world = numpy.zeros((NUMBER_OF_ROWS,NUMBER_OF_COLS))
+#world[0,3] = 1
+#world[1,3] = -1
+world[9,9] = 1
+world[NUMBER_OF_ROWS-1,NUMBER_OF_COLS-1]
 exploring = []
 
 
 def neighbors_of(row,col):
-    if row == 0 and col == 3:
-        return []
-    if row == 1 and col == 3:
+    if row == 9 and col == 9:
         return []
     result = []
     # for up action
     if row - 1 >= 0:
         result.append((row-1,col))
     # for down action
-    if row + 1 < 3:
+    if row + 1 < NUMBER_OF_ROWS:
         result.append((row+1,col))
     # for left action
     if col - 1 >= 0:
         result.append((row,col-1))
     # for down action
-    if col + 1 < 4:
+    if col + 1 < NUMBER_OF_COLS:
         result.append((row,col+1))
     return result
 
 
+Reward = numpy.zeros((NUMBER_OF_ROWS,NUMBER_OF_COLS))
+
 def value(row,col):
     if (row,col) in exploring:
         return world[row,col]
+    if Reward[row,col] != 0:
+        return Reward[row, col]
     exploring.append((row,col))
     neighbors = neighbors_of(row,col)
     result = []
@@ -40,10 +46,14 @@ def value(row,col):
         return world[row,col] + 0.9 * numpy.max(result)
     return world[row,col]
 
-print(value(0,0))
+
+exploring = []
+print(value(9,8))
+exploring = []
+print(value(0,1))
 
 
-Reward = numpy.zeros((3,4))
+
 for row in range(len(world)):
     for col in range(len(world[row])):
         exploring = []
